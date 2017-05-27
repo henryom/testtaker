db917286a1f26a4f5444efcde1ac1cfdf4d72252
Aletea <299880@bsd48.org>
2017-05-27 15:50:22 -0700

    Create

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,43 +1,17 @@
 package com.test;
 
+
 import java.util.Scanner;
+import java.lang.Math.random();
+
 
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+	// write your code here
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Type test to print a file.  Type read to read into an array.  Type create to create a test.");
+        System.out.println("Type TEST to print a file.  Type READ to read into an array.  Type CREATE to create a test.");
         String command = scanner.nextLine();
-        command = command.toLowerCase();
-        switch (command) {
-            case "test":
-                test();
-                break;
-            case "read":
-                read();
-                break;
-            case "create":
-                create();
-                break;
-            default:
-                System.out.println("No command named: " + command + " Type help for help.");
-        }
-        scanner.close();
     }
 
-    private static void test() {
-        System.out.println("test");
-    }
-
-
-=======
-    private static void read() {
-        System.out.println("read");
-    }
-
-    private static void create() {
-        System.out.println("create");
-    }
 }
->>>>>>> 516deadfffbc7d7a25917cc900ea7aaaba750921
