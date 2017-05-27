9239b84e512a8af4504f0b95a566fe5c917282c8
Henry O'Scannlain-Miller <henryom@me.com>
2017-05-27 14:38:40 -0700

    added a scanner

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,9 +1,13 @@
 package com.test;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hellow World");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type TEST to print a file.  Type READ to read into an array.  Type CREATE to create a test.");
+        String command = scanner.nextLine();
     }
 }
