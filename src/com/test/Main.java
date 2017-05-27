233839ca61e05f53334b066595bf6a668e5391ab
Henry O'Scannlain-Miller <henryom@me.com>
2017-05-27 14:52:51 -0700

    now reads commands from console

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -9,5 +9,33 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Type TEST to print a file.  Type READ to read into an array.  Type CREATE to create a test.");
         String command = scanner.nextLine();
+        command = command.toLowerCase();
+        switch (command){
+            case "test":
+                test();
+                break;
+            case "read":
+                read();
+                break;
+            case "create":
+                create();
+                break;
+            default:
+                System.out.println("No command named: " + command + " Type help for help.");
+        }
+        scanner.close();
     }
+
+    private static void test(){
+
+    }
+
+    private static void read(){
+
+    }
+
+    private static void create(){
+
+    }
+
 }
